f9e68b45594bd60e2d077b2dcc4e0d78870b7809
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 10:13:23 +0200

    Added BankAccounts class

---
+++ ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class BankAccount
+    {
+        public class BankAccount
+        {
+            public string Number { get; }
+            public string Owner { get; set; }
+            public decimal Balance { get; }
+
+            public void MakeDeposit(decimal amount, DateTime date, string note)
+            {
+            }
+            public voide MakeWithdrawal(decimal amount, DateTime date, string note)
+            {
+            }
+    }
+}
