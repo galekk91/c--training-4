c5d4acc3900415f05221370c29b1ea5957656b74
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 10:46:51 +0200

    Updated methods in BankAccount.cs

--- ConsoleApp1/ConsoleApp1/BankAccount.cs
+++ ConsoleApp1/ConsoleApp1/BankAccount.cs
@@ -6,23 +6,51 @@ namespace ConsoleApp1
 {
     public class BankAccount
     {
+        private List<Transaction> allTransactions = new List<Transaction>();
         private static int AccountNumberSeed = 1234567890;
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                }
+                return balance;
+            }
+        }
         public BankAccount(string name, decimal InitialBalance)
         {
             this.Owner = name;
-            this.Balance = InitialBalance;
+            MakeDeposit(InitialBalance, DateTime.Now, "Initial balance");
             this.Number = AccountNumberSeed.ToString();
             AccountNumberSeed++;
         }
 
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance-amount <0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(amount, date, note);
+            allTransactions.Add(withdrawal);
         }
     }
 }
--- ConsoleApp1/ConsoleApp1/Transaction.cs
+++ ConsoleApp1/ConsoleApp1/Transaction.cs
@@ -4,7 +4,18 @@ using System.Text;
 
 namespace ConsoleApp1
 {
-    class Class1
+    class Transaction
     {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+
     }
 }
