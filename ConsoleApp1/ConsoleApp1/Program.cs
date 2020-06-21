f2c06f8582575b23531078a7f233a8d1c11e9a2e
Dawid Gal <d.gal@poczta.onet.pl>
2020-06-21 10:56:59 +0200

    Finished the exercisex

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,6 +8,12 @@ namespace ConsoleApp1
         {
             var account = new BankAccount("Dawid", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "friend paid me back");
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
